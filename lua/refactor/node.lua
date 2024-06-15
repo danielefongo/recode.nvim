1fc3b94e67974a0945d23648d8fbf372cbb6b464
danielefongo <danielefongo@gmail.com>
2024-06-15 07:04:45 +0200

    feat(node): find inside and outside

--- lua/refactor/node.lua
+++ lua/refactor/node.lua
@@ -6,6 +6,20 @@
 local M = {}
 M.__index = M
 
+---@return Node[]
+---@param nodes Node[]
+---@param lambda function
+---@param filter function | nil
+local function filter_nodes(nodes, lambda, filter)
+  local filter_fun = filter or function(_)
+    return true
+  end
+
+  return vim.tbl_filter(function(node)
+    return lambda(node) and filter_fun(node)
+  end, nodes)
+end
+
 ---@return Node
 ---@param type string
 ---@param range Range
@@ -20,6 +34,38 @@ function M.new(type, range, node, text)
   self.text = text
 
   return self
+end
+
+---@return Node
+---@param range Range
+---@param node_type string | nil
+function M.dummy(range, node_type)
+  local self = setmetatable({}, M)
+
+  self.range = range
+  self.type = node_type or ""
+
+  return self
+end
+
+---@return Node[]
+---@param nodes Node[]
+---@param node_type string
+---@param filter function | nil
+function M:find_inside(nodes, node_type, filter)
+  return filter_nodes(nodes, function(node)
+    return self.range:contains_range(node.range) and node.type == node_type
+  end, filter)
+end
+
+---@return Node[]
+---@param nodes Node[]
+---@param node_type string
+---@param filter function | nil
+function M:find_outside(nodes, node_type, filter)
+  return filter_nodes(nodes, function(node)
+    return node.range:contains_range(self.range) and node.type == node_type
+  end, filter)
 end
 
 return M
---
+++ lua/tests/refactor/node_spec.lua
@@ -0,0 +1,63 @@
+local helpers = require("tests.helpers")
+local Range = require("refactor.range")
+local Node = require("refactor.node")
+
+describe("node", function()
+  helpers.setup()
+
+  describe("search", function()
+    local a = Node.dummy(Range.new(0, 0, 40, 0), "0")
+    local b = Node.dummy(Range.new(10, 0, 20, 0), "1")
+    local c = Node.dummy(Range.new(20, 0, 30, 0), "1")
+    local d = Node.dummy(Range.new(15, 0, 18, 0), "2")
+    local e = Node.dummy(Range.new(15, 0, 30, 0), "2")
+
+    local nodes = { a, b, c, d, e }
+
+    it("inside", function()
+      assert.are.same({}, a:find_inside(nodes, "0"))
+      assert.are.same({ b, c }, a:find_inside(nodes, "1"))
+      assert.are.same({ d, e }, a:find_inside(nodes, "2"))
+      assert.are.same({}, b:find_inside(nodes, "1"))
+      assert.are.same({ d }, b:find_inside(nodes, "2"))
+      assert.are.same({}, c:find_inside(nodes, "1"))
+      assert.are.same({}, c:find_inside(nodes, "2"))
+      assert.are.same({}, d:find_inside(nodes, "1"))
+      assert.are.same({}, d:find_inside(nodes, "2"))
+    end)
+
+    it("outside", function()
+      assert.are.same({}, a:find_outside(nodes, "0"))
+      assert.are.same({}, a:find_outside(nodes, "1"))
+      assert.are.same({}, a:find_outside(nodes, "2"))
+      assert.are.same({ a }, b:find_outside(nodes, "0"))
+      assert.are.same({}, b:find_outside(nodes, "1"))
+      assert.are.same({}, b:find_outside(nodes, "2"))
+      assert.are.same({ a }, c:find_outside(nodes, "0"))
+      assert.are.same({}, c:find_outside(nodes, "1"))
+      assert.are.same({ e }, c:find_outside(nodes, "2"))
+      assert.are.same({ a }, d:find_outside(nodes, "0"))
+      assert.are.same({ b }, d:find_outside(nodes, "1"))
+      assert.are.same({ e }, d:find_outside(nodes, "2"))
+      assert.are.same({ a }, e:find_outside(nodes, "0"))
+      assert.are.same({}, e:find_outside(nodes, "1"))
+      assert.are.same({}, e:find_outside(nodes, "2"))
+    end)
+
+    it("with filter", function()
+      assert.are.same(
+        { b },
+        a:find_inside(nodes, "1", function(n)
+          return vim.deep_equal(n, b)
+        end)
+      )
+
+      assert.are.same(
+        {},
+        b:find_inside(nodes, "0", function(n)
+          return not vim.deep_equal(n, a)
+        end)
+      )
+    end)
+  end)
+end)
