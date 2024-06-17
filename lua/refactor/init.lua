7cdd4d5844a495d35d75e46c298d1e4a5746847c
danielefongo <danielefongo@gmail.com>
2024-06-17 13:33:17 +0200

    feat: handle visual selection

--- lua/refactor/init.lua
+++ lua/refactor/init.lua
@@ -15,11 +15,27 @@ M.lenses:register_many({
   require("tests.example.swap_parameter"),
 })
 
+local function get_range()
+  local mode = vim.fn.mode()
+  if mode == "v" or mode == "V" or mode == "" or vim.fn.visualmode() ~= "" then
+    local _, start_row, start_col, _ = unpack(vim.fn.getpos("'<"))
+    local _, end_row, end_col, _ = unpack(vim.fn.getpos("'>"))
+    return Range.from_vim({ start_row - 1, start_col - 1, end_row - 1, end_col - 1 })
+  else
+    local vim_cursor = vim.api.nvim_win_get_cursor(0)
+    local cursor = Cursor.from_vim(vim_cursor)
+    return Range.from_cursor(cursor)
+  end
+end
+
 function M.spike()
   local buffer = vim.api.nvim_get_current_buf()
-  local cursor = Cursor.from_vim(vim.api.nvim_win_get_cursor(0))
-  local range = Range.from_cursors(cursor, cursor)
+  local range = get_range()
   local refactors = M.lenses:suggestions(buffer, range)
+
+  if #refactors == 0 then
+    return
+  end
 
   vim.ui.select(
     vim.tbl_map(function(refactor)
